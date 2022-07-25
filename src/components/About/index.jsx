14ba88684bc407c5047e141d4109149b376f8ae2
harshsiri <harshsiriah1@gmail.com>
2022-07-25 10:55:28 +0530

    Changed contact page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,32 +1,18 @@
 import React from "react";
-import { Image, Stack } from "react-bootstrap";
+import { Col, Image, Stack } from "react-bootstrap";
 import colors from "../../utils/colors";
-import copyTexts from "../../utils/copyTexts";
-import { ReactComponent as LinkedInLogo } from "../../assets/linkedin-logo.svg";
-import { ReactComponent as GithubLogo } from "../../assets/github-logo.svg";
-import { Links } from "../../utils/links";
-
-const profImage = require("../../assets/Prof_Image.png");
 
 function About() {
   return (
     <Stack gap={5} style={styles.container}>
       <h1 style={styles.heading}>ABOUT ME</h1>
-      <Stack direction="horizontal" gap={5}>
-        <Image roundedCircle={true} src={profImage} style={styles.profImage} />
-        <div style={styles.separator} />
-        <p className="fs-6" style={styles.aboutText}>
-          {copyTexts.aboutText}
-        </p>
-        <Stack gap={5} style={styles.container}>
-          <a href={Links.LinkedIn} target="_blank">
-            <LinkedInLogo fill={colors.cadetBlue} />
-          </a>
-          <a href={Links.Github} target="_blank">
-            <GithubLogo fill={colors.cadetBlue} />
-          </a>
-        </Stack>
-      </Stack>
+      <Col
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      ></Col>
     </Stack>
   );
 }
@@ -51,8 +37,8 @@ const styles = {
     height: "333px",
   },
   separator: {
-    height: "255px",
-    width: "4px",
+    width: "80vw",
+    height: "4px",
     backgroundColor: "#C4C4C4BF",
     borderRadius: "10px",
   },
