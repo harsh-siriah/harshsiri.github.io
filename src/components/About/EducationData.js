25aae7fa14804ade678f222e6b166ab53e92139b
harshsiri <harshsiriah1@gmail.com>
2022-08-23 19:43:57 +0530

    Added education component

---
+++ src/components/About/EducationData.js
@@ -0,0 +1,31 @@
+const data = {
+  items: ["vit", "jog", "gurukul"],
+  data: {
+    vit: {
+      id: "vit",
+      year: "2021",
+      name: "Vishwakarma Institute of Technology, Pune",
+      degree: "Bachelor of Technology - Computer Engineering",
+      url: "https://www.vit.edu/",
+      content: ["GPA - 9.51"],
+    },
+    jog: {
+      id: "jog",
+      year: "2017",
+      name: "P. Jog Junior College, Pune",
+      degree: "Senior Secondary - Science",
+      url: "https://pjogcolleges.com/",
+      content: ["Grade - 80%"],
+    },
+    gurukul: {
+      id: "gurukul",
+      year: "2015",
+      name: "S.E.S Gurukul School, Pune",
+      degree: "Secondary",
+      url: "https://gurukulpune.edu.in/",
+      content: ["CGPA - 8"],
+    },
+  },
+};
+
+export default data;
--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import colors from "../../utils/colors";
-import { education } from "../../assets";
+import "./AboutScreen.css";
+import EducationData from "./EducationData";
 
 function About() {
   return (
     <div style={styles.container} key="About">
       <h1 style={styles.heading}>ABOUT ME</h1>
 
-      <img src={education} style={styles.education} alt="Education" />
+      <div className="education-box">
+        <div className="year-cell-dotted-border" />
+        <div className="year-cell-container">
+          {EducationData.items.map((item, index) => {
+            const currentItemData = EducationData.data[item];
+            return (
+              <div className="year-cell-content" key={currentItemData.id}>
+                <div className="year-cell">
+                  <p className="year-cell-text">{currentItemData.year}</p>
+                </div>
+                <div className="year-content-container">
+                  <a
+                    href={currentItemData.url}
+                    className="institute-name-text"
+                    target={"_blank"}
+                  >
+                    {currentItemData.name}
+                  </a>
+                  <p className="degree-text">{currentItemData.degree}</p>
+                  <p className="content-text">{currentItemData.content}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
